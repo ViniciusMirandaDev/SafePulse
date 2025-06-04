59056f8607222a95738932fd3768f8c859634fd7
ViniciusMirandaDev <vinimb07@gmail.com>
2025-06-04 18:22:08 -0300

    feat: initializing project

---
+++ src/main/java/br/org/fiap/safepulse/SafepulseApplication.java
@@ -0,0 +1,13 @@
+package br.org.fiap.safepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafepulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafepulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/fiap/safepulse/SafepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.fiap.safepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
